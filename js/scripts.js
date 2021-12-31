6c9206f209873ffca3078c5bf0a2532f061caf29
Pedro Henrique <henriquegostoso0204@gmail.com>
2021-12-31 07:04:48 -0300

    modal

--- js/scripts.js
+++ js/scripts.js
@@ -63,12 +63,25 @@ _elements.date.innerHTML = new Date().getFullYear() + '.'
 
 _elements.scrollLinks.forEach(link => {
   link.addEventListener('click', e => {
-    const id = link.getAttribute("href");
-		console.log(id)
+    _elements.navbarList.classList.remove('navbar-list--show-links')
+
+    const id = link.getAttribute('href')
+    const element = document.querySelector(id)
+
+    const position = element.offsetTop - 62
+
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth'
+    })
+
+    e.preventDefault()
   })
 })
 
-_elements.toggle.addEventListener('click', () => {})
+_elements.toggle.addEventListener('click', () => {
+  _elements.navbarList.classList.toggle('navbar-list--show-links')
+})
 
 _elements.galleryItems.forEach(item => {})
 
